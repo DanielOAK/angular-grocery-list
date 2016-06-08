d27389231c12ec5012fafbd64ad569c8ca7ce26f
Daniel Humbert <achristian4truth@yahoo.com>
2016-06-07 20:28:26 -0500

    delete item

--- scripts/main.js
+++ scripts/main.js
@@ -24,17 +24,28 @@ app.controller('MainCtrl', ['$scope', function($scope) {
       price: 2
     };
     $scope.groceryList.push(item);
+    $scope.itemName = "";
     console.log("it works!");
   };
-  $scope.deleteItem = function() {
-    var item = {
-      name: $scope.itemName,
-      price: 1
-    };
-    $scope.groceryList.splice(item);
+  $scope.deleteItem = function(index) {
+    // Get the index of the clicked item
+    // delete one item from the array starting at that index
+    console.log(index);
+    $scope.groceryList.splice(index, 1);
     console.log("did it!");
   };
 }]);
+
+// var buttons = document.getElementsByClassName("glyphicon-trash");
+// for (var i = 0; i < buttons.length; i++) {
+//   buttons[i].addEventListener("mousedown", function() {
+//
+//   });
+// }
+//
+// $(".glyphicon-trash").click(function() {
+//
+// });
 
 
 //$scope.item.splice($scope.item.indexOf($scope.item), 1);
